8bb9b175819ac3b638014f7008c7a96cce8cdd4c
Aaron-Doran <wesley.gossett@gmail.com>
2020-02-21 23:31:15 -0500

    having trouble with login

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,143 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
 
-const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+const initialColor = {
+  color: "",
+  code: { hex: "" }
+};
+
+const ColorList = ({ colors, updateColors }) => {
+  console.log(colors);
+  const [editing, setEditing] = useState(false);
+  const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [newColor, setNewColor] = useState(initialColor);
+
+  const editColor = color => {
+    setEditing(true);
+    setColorToEdit(color);
+  };
+
+  const addColorChangeHandler = e => {
+    setNewColor({ ...newColor, color: e.target.value });
+  };
+
+  const addHexChangeHandler = e => {
+    setNewColor({
+      ...newColor,
+      code: { hex: e.target.value }
+    });
+  };
+
+  const addColor = color => {
+    axiosWithAuth()
+      .post('/colors', color)
+      .then(res => console.log('You added a color!', newColor))
+      .catch(err => console.log(err));
+  }
+
+  const saveEdit = e => {
+    e.preventDefault();
+    const body = { ...colorToEdit };
+    const { id } = colorToEdit;
+    axiosWithAuth()
+      .put(`/colors/${id}`, body)
+      .catch(err => console.log(err));
+    setEditing(false);
+    grabColors();
+  };
+
+  const grabColors = () => {
+    setTimeout(() => {
+      axiosWithAuth()
+        .get('/colors')
+        .then(res => updateColors(res.data))
+        .catch(err => console.log(err));
+    }, 100);
+  }
+
+  const deleteColor = color => {
+    axiosWithAuth()
+      .delete(`/colors/${color.id}`)
+      .then(res => {
+        console.log(res);
+        grabColors();
+      })
+      .catch(err => console.log(err.res));
+  };
 
   return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
+    <div className="colors-wrap">
+      <p>colors</p>
+      <ul>
+        {colors.map(color => (
+          <li key={color.color} onClick={() => editColor(color)}>
+            <span>
+              <span className="delete" onClick={e => {
+                    e.stopPropagation();
+                    deleteColor(color)
+                  }
+                }>
+                  x
+              </span>{" "}
+              {color.color}
+            </span>
+            <div
+              className="color-box"
+              style={{ backgroundColor: color.code.hex }}
+            />
+          </li>
+        ))}
+      </ul>
+      {editing && (
+        <form onSubmit={saveEdit}>
+          <legend>edit color</legend>
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setColorToEdit({ ...colorToEdit, color: e.target.value })
+              }
+              value={colorToEdit.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setColorToEdit({
+                  ...colorToEdit,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorToEdit.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button type="submit">save</button>
+            <button onClick={() => setEditing(false)}>cancel</button>
+          </div>
+        </form>
+      )}
+      {/* <div className="spacer" /> */}
+      {/* stretch - build another form here to add a color */}
+      <form onSubmit={() => addColor(newColor)}>
+        <legend>add color</legend>
+        <label>
+          color name:
+          <input onChange={addColorChangeHandler} value={newColor.color} />
+        </label>
+        <label>
+          hex code:
+          <input onChange={addHexChangeHandler} value={newColor.code.hex} />
+        </label>
+        <div className='button-row'>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
-export default BubblePage;
+export default ColorList;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useState} from "react";
 
-const Login = () => {
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+import { Form, Button } from 'react-bootstrap'
+
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [user, setUser] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setUser ({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const login = e => {
+    e.prevetnDefault();
+    axiosWithAuth()
+    .post('/login', user)
+    .then( res => {
+      console.log(res);
+      localStorage.setItem('token', res.data.payload);
+      props.history.push('/bubbles');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <Form onSubmit={login}>
+        <Form.Group controlId='formBasicUsername'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control 
+          type='username'
+          name='username'
+          value={user.username}
+          onChange={handleChange} 
+          placeholder='Enter username' />
+        </Form.Group>
+
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control 
+          type='password'
+          name='password'
+          value={user.password}
+          onChange={handleChange} 
+          placeholder='Enter password' />
+        </Form.Group>
+        <Button variant='primary' type='submit'>Login</Button>
+      </Form>
     </>
   );
 };
